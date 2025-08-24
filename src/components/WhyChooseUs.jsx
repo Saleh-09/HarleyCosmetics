5650f960ac000cc683575c3fd9625615e06e1c21
Saleh <saleharain03@gmail.com>
2025-08-24 19:50:38 +0500

    feat: update image size in WhyChooseUs component for responsive design

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -16,7 +16,7 @@ const WhyChooseUs = ({
           <img
             src={image}
             alt={title}
-            className="w-[648px] h-[491px] object-cover rounded-2xl shadow-lg"
+            className="w-[265px] md:w-[648px] md:h-[491px] h-auto object-cover rounded-2xl shadow-lg"
           />
         </div>
 
