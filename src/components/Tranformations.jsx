4e848428c094e2809f17bb4fa9dedf4eab03e6d8
Saleh <saleharain03@gmail.com>
2025-08-28 09:18:07 +0500

    Upated Patient Images

--- src/components/Tranformations.jsx
+++ src/components/Tranformations.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import FirstTransformation from "../assets/Tranformation1.svg"
+import Vertical from "../assets/PatientImages/Vertifcal Frames.png"
+import Horizontal from "../assets/PatientImages/Horizontal Frames.png"
 const Transformations=()=>{
     return(
         <div className="max-w-7xl mx-auto px-4 mb-15">
@@ -16,7 +18,7 @@ const Transformations=()=>{
                       src={FirstTransformation}
                       alt="firstTranformation"
                     />
-                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Beard Transplant</h2>
+                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Hair Transplant</h2>
                     <p className="text-gray-600 text-center leading-relaxed">
                         2,000 Grafts| Donor Area: Back of scalp | Result Time: 8 Months
                     </p>
@@ -24,10 +26,10 @@ const Transformations=()=>{
 
                 <div>
                     <img className="mb-3"
-                      src={FirstTransformation}
+                      src={Vertical}
                       alt="firstTranformation"
                     />
-                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Beard Transplant</h2>
+                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Hair Transplant</h2>
                     <p className="text-gray-600 text-center leading-relaxed">
                         2,000 Grafts| Donor Area: Back of scalp | Result Time: 8 Months
                     </p>
@@ -35,10 +37,10 @@ const Transformations=()=>{
 
                 <div>
                     <img className="mb-3"
-                      src={FirstTransformation}
+                      src={Horizontal}
                       alt="firstTranformation"
                     />
-                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Beard Transplant</h2>
+                    <h2 className="text-xl font-semibold text-gray-900 mb-2 text-center">Hair Transplant</h2>
                     <p className="text-gray-600 text-center leading-relaxed">
                         2,000 Grafts| Donor Area: Back of scalp | Result Time: 8 Months
                     </p>
